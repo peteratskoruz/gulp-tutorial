674d1a04ee3f7f0b0df483b30740ef271f769142
peteratskoruz <peterjoeeraj234@gmail.com>
2018-04-27 16:04:48 +0530

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,40 @@
+
+(function ($) {
+
+    //toggle humburger
+
+    $('.toggle-btn').click(function() {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+    });
+
+    // Navigation scrolls
+    $('.navbar-nav li a').bind('click', function(event) {
+        $('.navbar-nav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        var $anchor = $(this);
+        var nav = $($anchor.attr('href'));
+        if (nav.length) {
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+
+            event.preventDefault();
+        }
+    });
+
+    // About section scroll
+    $(".overlay-detail a").on('click', function(event) {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900, function(){
+            window.location.hash = hash;
+        });
+    });
+
+    console.log('Awesome!');
+})(jQuery);
+
+
